60077abfd66f71c7b676a328a1977853b48ac17c
patrickbindelli <patrickvenanbindelli@gmail.com>
2025-08-06 18:54:37 -0300

    feat: novo payload no jwt service

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -1,39 +1,36 @@
 import jwt from 'jsonwebtoken';
 import config from '../config';
-
-interface userPayload {
-  id: number;
-}
+import { UserTokenPayload } from '@/schemas/user.schemas';
 
 export class JwtService {
   private readonly ACCESS_TOKEN_SECRET = config.access_token_secret;
   private readonly REFRESH_TOKEN_SECRET = config.refresh_token_secret;
 
-  public generateAccessToken(userPayload: userPayload): string {
+  public generateAccessToken(userPayload: UserTokenPayload): string {
     return jwt.sign(userPayload, this.ACCESS_TOKEN_SECRET, {
       expiresIn: '15m',
     });
   }
 
-  public generateRefreshToken(userPayload: userPayload): string {
+  public generateRefreshToken(userPayload: UserTokenPayload): string {
     return jwt.sign(userPayload, this.REFRESH_TOKEN_SECRET, {
       expiresIn: '7d',
     });
   }
 
-  public verifyToken(token: string, secret: string): userPayload | null {
+  public verifyToken(token: string, secret: string): UserTokenPayload | null {
     try {
-      return jwt.verify(token, secret) as userPayload;
+      return jwt.verify(token, secret) as UserTokenPayload;
     } catch (error) {
       return null;
     }
   }
 
-  public verifyAccessToken(token: string): userPayload | null {
+  public verifyAccessToken(token: string): UserTokenPayload | null {
     return this.verifyToken(token, this.ACCESS_TOKEN_SECRET);
   }
 
-  public verifyRefreshToken(token: string): userPayload | null {
+  public verifyRefreshToken(token: string): UserTokenPayload | null {
     return this.verifyToken(token, this.REFRESH_TOKEN_SECRET);
   }
 }
